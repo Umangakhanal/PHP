3c051cf4ed4f5794a3f04fa69f3d32b7fdecd05e
umangakhanal <umanga.khanal@excelcollege.edu.np>
2025-11-10 08:46:33 +0545

    loops (while, do-while, for )

---
+++ loop.php
@@ -0,0 +1,21 @@
+<?php
+$i=1;
+$a=1;
+// While loop
+while($i<=5){
+    echo $i;
+    $i++;
+    break;
+}
+
+//Do loop
+do{
+    echo $a;
+    $a++;
+} while($a<=5);
+
+//for loop
+for($a=1; $a<=5;$a++){
+    echo  $a;
+}
+?>
