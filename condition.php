999dce6e20b477350a91b7251fd7129721e9ff9f
umangakhanal <umanga.khanal@excelcollege.edu.np>
2025-11-10 08:01:29 +0545

    first commit

---
+++ condition.php
@@ -0,0 +1,8 @@
+<?php
+//Ternary Operator  ?=condition  :=Assignment/ seperator if true:if false
+$stream="BCSIT";
+
+// echo $stream?"BCSIT":"BBA"
+// echo ($stream=="BCSIT")?"BCSIT":"BBA";
+
+?>
---
+++ index.php
@@ -0,0 +1,54 @@
+ <?php
+// echo "Hello";
+// echo '<\html>
+// <body>
+    
+//     <h1>HI! PHP</h1>
+// </body>
+// </html>';
+
+// Variable 
+//  $a="HELOO";
+//  echo $a;
+    // const b=35;
+    // echo b ."<br>";
+    
+    // $a=b;
+    // echo  $a ."<br>" ;
+
+    // $a=40;
+    // echo $a ."<br>";
+
+ 
+// phpinfo();
+
+//  $a="Ram";
+//  echo '$a'."\n";
+//  echo "$a"."<br>";
+//  echo $a;
+// echo 'hi,'.$a;
+                       //Conditional   if - else  - else if 
+// $day="sunday";
+// if($day=="saturday"){
+//     echo "Holiday";
+// }
+// else{
+//     echo "weekday";
+// }
+   $stream= "BCSIT";
+   switch($stream){
+    case "BBA": 
+        echo "BBA choosen";
+        break;
+
+    case "BCSIT":
+        echo "BCSIT choosen";
+        break;
+    
+    default:
+    echo "Nothing";
+   }
+
+
+
+?> 
