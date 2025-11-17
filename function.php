bd598383c2f75e6e40ed89c76e9daf6f65b9330c
umangakhanal <umanga.khanal@excelcollege.edu.np>
2025-11-17 09:03:11 +0545

    Array

--- function.php
+++ function.php
@@ -1,11 +1,28 @@
 <?php
-$sum=123;
-function sum(int $a , int $b ){
+// $sum=123;
+// function sum(int $a , int $b ){
 
-    $add=$a+$b;
-    // echo $add;
-    return $add;
-}
-echo sum(2,b: 3);
-echo $sum;
+//     $add=$a+$b;
+//     // echo $add;
+//     return $add;
+// }
+// echo sum(2, 3);
+// echo $sum;
+
+
+//hashing 
+// echo hash("sha256", "Bhaskar ojha ");
+// echo "<br>";
+// echo md5("Bhaskar ojha ");
+
+$course = array("BBA", "BCSIT","BE");
+var_dump($course) ;
+print_r($course);
+print_r($course[1]);
+ echo "<br>";
+
+ foreach($course as $program){
+    echo "$program <br>";
+ }
+
 ?>
