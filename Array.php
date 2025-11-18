3270bdc57021c4d916670adfbcb3b7598007f54b
umangakhanal <umanga.khanal@excelcollege.edu.np>
2025-11-18 08:34:01 +0545

    associative array

---
+++ Array.php
@@ -0,0 +1,23 @@
+<?php 
+$x=1;
+echo "$x <br>";
+echo '$x <br>';
+//above 3 lines are to show difference between "" and '' 
+$a=array(1,'2','3','4','5');
+print_r($a);
+echo "<br>";
+var_dump($a);
+//types of array
+// indexed array
+// associative array
+// multidimensional array
+
+//associative array
+$student =array("name"=>"umanga", "age"=>20,"address"=>"kathmandu");
+print_r($student);
+echo"<br>";
+var_dump($student);
+
+//multidimensional array
+
+?>
